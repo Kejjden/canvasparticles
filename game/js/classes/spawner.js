ebef3dc09abe425f04afb36aa55e6d8a2bc63587
chips <chips@metrica.se>
2015-07-17 21:10:53 +0200

    sort sprites and stuff

--- game/js/classes/spawner.js
+++ game/js/classes/spawner.js
@@ -5,18 +5,18 @@ var Spawner = (function () {
         this.counter = 0;
         this.newWave = function () {
             _this.activeDoor = _this.getRandomDoor();
-            console.log("door open!");
+            l("door open!");
             _this.activeDoor.state = 1;
             _this.counter++;
             _this.zombieCounter = Math.floor(Math.exp(_this.counter) * 0.5);
-            console.log("spawncount: " + _this.zombieCounter);
+            l("spawncount: " + _this.zombieCounter);
             _this.nextZombie();
         };
         this.nextZombie = function () {
-            console.log("zombies remaining: " + _this.zombieCounter);
+            l("zombies remaining: " + _this.zombieCounter);
             _this.cancel();
             var pirate = new ZombiePirate(_this.activeDoor.x, _this.activeDoor.y);
-            _this.game.zombiepirates.push(pirate);
+            _this.game.entities.push(pirate);
             _this.zombieCounter--;
             if (_this.zombieCounter == 0) {
                 _this.timer = new Timer(1000, _this.closeDoor);
@@ -28,15 +28,15 @@ var Spawner = (function () {
             }
         };
         this.closeDoor = function () {
-            console.log("closing door");
+            l("closing door");
             _this.activeDoor.state = 0;
             _this.cancel();
             _this.activeDoor = null;
-            if (_this.counter < 5) {
+            if (_this.counter < 2) {
                 _this.nextWave();
             }
             else {
-                console.log("you win111!");
+                l("you win111!");
             }
         };
         this.nextWave();
@@ -67,11 +67,11 @@ var Spawner = (function () {
     };
     Spawner.prototype.nextWave = function () {
         if (this.timer) {
-            console.log("oh poop");
+            l("oh poop");
             return;
         }
         var delay = Math.max(2000, 1000 + this.counter * 5000);
-        console.log("new wave in: " + delay);
+        l("new wave in: " + delay);
         this.timer = new Timer(delay, this.newWave);
         this.timer.resume();
     };
